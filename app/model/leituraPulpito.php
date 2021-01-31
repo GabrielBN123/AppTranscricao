eae476de17817549edbb064e579d53036d2a612d
GabrielBN123 <gabriel.nogueira@senaimt.edu.br>
2021-01-31 04:45:45 -0400

    Leitura de dados do formulário

---
+++ app/model/leituraPulpito.php
@@ -0,0 +1,93 @@
+<?php
+
+require('conexao.php');
+//namespace app\model\Conexao;
+
+class LeituraFormulário extends Conexao
+{
+    private $select = null;
+
+    public function selectDados()
+    {
+        $this->select = $this->conexao()->prepare('SELECT * FROM formulario');
+    }
+
+    public function getDados()
+    {
+        return $this->select->execute();
+    }
+
+    public function exibirDados()
+    {
+        $select = $this->conexao()->prepare('SELECT * FROM formulario');
+
+        $select->execute();
+
+        foreach ($select as $texto) {
+
+            if ($texto['apresentacao'] != null || '') {
+?>
+                <div class="col-md-12">
+                    <h5>Apresentação</h5>
+                    <p class="py-2 px-2 border border-primary rounded" style="text-align: justify;">
+                        <?php echo $texto['apresentacao']; ?>
+                    </p>
+                </div>
+            <?php
+            }
+
+            if ($texto['aviso'] != null || '') {
+            ?>
+                <div class="col-md-12">
+                    <h5>Avisos</h5>
+                    <p class="py-2 px-2 border border-primary rounded" style="text-align: justify;">
+                        <?php echo $texto['aviso']; ?>
+                    </p>
+                </div>
+            <?php
+            }
+            if ($texto['cartaApresentacao'] != null || '') {
+            ?>
+                <div class="col-md-12">
+                    <h5>Carta de apresentação</h5>
+                    <p class="py-2 px-2 border border-primary rounded" style="text-align: justify;">
+                        <?php echo $texto['cartaApresentacao']; ?>
+                    </p>
+                </div>
+            <?php
+            }
+            if ($texto['acaoGraca'] != null || '') {
+            ?>
+                <div class="col-md-12">
+                    <h5>Ação de Graças</h5>
+                    <p class="py-2 px-2 border border-primary rounded" style="text-align: justify;">
+                        <?php echo $texto['acaoGraca']; ?>
+                    </p>
+                </div>
+            <?php
+            }
+            if ($texto['pedidoOracao'] != null || '') {
+            ?>
+                <div class="col-md-12">
+                    <h5>Pedido de Oração</h5>
+                    <p class="py-2 px-2 border border-primary rounded" style="text-align: justify;">
+                        <?php echo $texto['pedidoOracao']; ?>
+                    </p>
+                </div>
+            <?php
+            }
+            if ($texto['apresentacaoRN'] != null || '') {
+            ?>
+                <div class="col-md-12">
+                    <h5>Apresentação de Recém-Nascidos</h5>
+                    <p class="py-2 px-2 border border-primary rounded" style="text-align: justify;">
+                        <?php echo $texto['apresentacaoRN']; ?>
+                    </p>
+                </div>
+<?php
+            }
+        }
+    }
+}
+$FormLeitura = new LeituraFormulário;
+$FormLeitura->selectDados();
--- app/view/forms/pulpito.php
+++ app/view/forms/pulpito.php
@@ -1,4 +1,6 @@
-
+<?php
+    include('../model/leituraPulpito.php');
+?>
 
 <title>Leitura</title>
 <div class="container">
@@ -8,73 +10,9 @@
             <div class="col-md-12 col-lg-12">
                 <div class="col-md-12">
                     <div class="row">
-                        <div class="col-md-12">
-                            <h5>Apresentação</h5>
-                            <p class="py-2 px-2 border border-primary rounded" style="text-align: justify;">
-                                Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-                                Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took
-                                a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but
-                                also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s
-                                with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing
-                                software like Aldus PageMaker including versions of Lorem Ipsum.
-                            </p>
-                        </div>
-                        <div class="col-md-12">
-                            <h5>Avisos</h5>
-                            <p class="py-2 px-2 border border-primary rounded" style="text-align: justify;">
-                                Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-                                Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took
-                                a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but
-                                also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s
-                                with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing
-                                software like Aldus PageMaker including versions of Lorem Ipsum.
-                            </p>
-                        </div>
-                        <div class="col-md-12">
-                            <h5>Carta de apresentação</h5>
-                            <p class="py-2 px-2 border border-primary rounded" style="text-align: justify;">
-                                Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-                                Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took
-                                a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but
-                                also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s
-                                with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing
-                                software like Aldus PageMaker including versions of Lorem Ipsum.
-                            </p>
-                        </div>
-                        <div class="col-md-12">
-                            <h5>Ação de Graças</h5>
-                            <p class="py-2 px-2 border border-primary rounded" style="text-align: justify;">
-                                Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-                                Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took
-                                a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but
-                                also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s
-                                with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing
-                                software like Aldus PageMaker including versions of Lorem Ipsum.
-                            </p>
-                        </div>
-
-                        <div class="col-md-12">
-                            <h5>Pedido de Oração</h5>
-                            <p class="py-2 px-2 border border-primary rounded" style="text-align: justify;">
-                                Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-                                Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took
-                                a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but
-                                also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s
-                                with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing
-                                software like Aldus PageMaker including versions of Lorem Ipsum.
-                            </p>
-                        </div>
-                        <div class="col-md-12">
-                            <h5>Apresentação de Recém-Nascidos</h5>
-                            <p class="py-2 px-2 border border-primary rounded" style="text-align: justify;">
-                                Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-                                Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took
-                                a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but
-                                also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s
-                                with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing
-                                software like Aldus PageMaker including versions of Lorem Ipsum.
-                            </p>
-                        </div>
+                        <?php
+                            $FormLeitura->exibirDados();0
+                        ?>
                         <hr>
                         <button class="col-md-12 col-lg-2 btn btn-primary btn-lg btn_form">Próximo</button>
                     </div>
