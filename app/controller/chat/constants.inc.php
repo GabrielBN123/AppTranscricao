48434e2bc767c80042e22ca3adeab8601e247fdb
GabrielBN123 <gabriel.nogueira@senaimt.edu.br>
2021-04-07 03:05:22 -0400

    config para desktop

--- app/controller/chat/constants.inc.php
+++ app/controller/chat/constants.inc.php
@@ -5,7 +5,8 @@ define ('DEFAULT_APP_DEBUG', 1);
 # Base de dados
 define('DB_HOSTNAME',   'localhost');
 define('DB_USERNAME',   'root');
-define('DB_PASSWORD',   'root');
+define('DB_PASSWORD',   '');
+// define('DB_PASSWORD',   'root');
 define('DB_NAME',       'transc_bd');
 
 # Tabelas
--- app/model/conexao.php
+++ app/model/conexao.php
@@ -6,18 +6,18 @@ class Conexao
 {
 
     // Job
-    private $c = [
-        'HOST' => 'localhost',
-        'PORT' => 3306,
-        'USER' => 'root',
-        'PASS' => 'root',
-        'DBNAME' => 'transc_bd',
-        'OPTIONS' => [
-            PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8',
-            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
-        ]
-    ];
+    // private $c = [
+    //     'HOST' => 'localhost',
+    //     'PORT' => 3306,
+    //     'USER' => 'root',
+    //     'PASS' => 'root',
+    //     'DBNAME' => 'transc_bd',
+    //     'OPTIONS' => [
+    //         PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8',
+    //         PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    //         PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+    //     ]
+    // ];
 
     // Conexão 02
     // private $c = [
@@ -32,6 +32,20 @@ class Conexao
     //         PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
     //     ]
     // ];
+
+    // Desk
+    private $c = [
+        'HOST' => 'localhost',
+        'PORT' => 3306,
+        'USER' => 'root',
+        'PASS' => '',
+        'DBNAME' => 'transc_bd',
+        'OPTIONS' => [
+            PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8',
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+        ]
+    ];
 
     // Conexão 03
     // private $c = [
